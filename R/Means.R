4fb5a3b230ce9ba7bbc377c9c5b6eb913f097f24
Georgina Anderson <georgina.anderson@phe.gov.uk>
2018-02-08 09:28:28 +0000

    Amended mean example to pass devtools check (comment out OR).  added RODBC to description suggests. removed tidyr library statement from DSR vignette

--- R/Means.R
+++ R/Means.R
@@ -17,7 +17,7 @@
 #' df <- data.frame(values = c(30,40,50,60))
 #' phe_mean(df, values)
 #'
-#' OR
+#' ## OR
 #'
 #' df2 <- data.frame(area = rep(c("Area1", "Area2"),each=3),
 #'                   values = c(20,30,40,200,300,400)) %>%
