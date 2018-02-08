bec8d79e8b2061b0b767022d27e8433dafc1add0
Georgina Anderson <georgina.anderson@phe.gov.uk>
2018-02-08 10:53:36 +0000

    documentation updated.  wilson testthat script - corrected numerators for CI error handling checks

--- tests/testthat/testWilson.R
+++ tests/testthat/testWilson.R
@@ -26,23 +26,23 @@ test_that("wilson_lower - errors are generated when invalid arguments are used",
                "sample sizes must all be greater than zero", info="wilson_lower error pop <= 0")
   expect_error(wilson_lower(c(65,80,1000),c(100,50,5000)),
                "numerators must be less than or equal to denominator for a Wilson score to be calculated", info="wilson_lower x > n")
-  expect_error(wilson_lower(c(65,80,1000),c(100,5000,1), confidence = 0.7),
+  expect_error(wilson_lower(c(65,80,1000),c(100,100,5000), confidence = 0.7),
                "confidence level must be between 90 and 100 or between 0.9 and 1", info="wilson_lower error confidence < 0.9")
-  expect_error(wilson_lower(c(65,80,1000),c(100,5000,1), confidence = 14),
+  expect_error(wilson_lower(c(65,80,1000),c(100,100,5000), confidence = 14),
                "confidence level must be between 90 and 100 or between 0.9 and 1", info="wilson_lower error confidence between 1 and 90")
-  expect_error(wilson_lower(c(65,80,1000),c(100,5000,1), confidence = 125),
+  expect_error(wilson_lower(c(65,80,1000),c(100,100,5000), confidence = 125),
                "confidence level must be between 90 and 100 or between 0.9 and 1", info="wilson_lower error confidence > 100")
-  expect_error(wilson_upper(c(65,0,-4),c(100,5000,1)),
+  expect_error(wilson_upper(c(65,0,-4),c(100,100,5000)),
                "observed events must all be greater than or equal to zero", info="wilson_upper error obs < 0")
-  expect_error(wilson_upper(c(65,80,1000),c(100,5000,-6)),
+  expect_error(wilson_upper(c(65,80,1000),c(100,100,-6)),
                "sample sizes must all be greater than zero", info="wilson_upper error pop <= 0")
   expect_error(wilson_upper(c(65,80,1000),c(100,50,5000)),
                "numerators must be less than or equal to denominator for a Wilson score to be calculated", info="wilson_upper x > n")
-  expect_error(wilson_upper(c(65,80,1000),c(100,5000,1), confidence = 0.7),
+  expect_error(wilson_upper(c(65,80,1000),c(100,100,5000), confidence = 0.7),
                "confidence level must be between 90 and 100 or between 0.9 and 1", info="wilson_upper error confidence < 0.9")
-  expect_error(wilson_upper(c(65,80,1000),c(100,5000,1), confidence = 14),
+  expect_error(wilson_upper(c(65,80,1000),c(100,100,5000), confidence = 14),
                "confidence level must be between 90 and 100 or between 0.9 and 1", info="wilson_upper error confidence between 1 and 90")
-  expect_error(wilson_upper(c(65,80,1000),c(100,5000,1), confidence = 125),
+  expect_error(wilson_upper(c(65,80,1000),c(100,100,5000), confidence = 125),
                "confidence level must be between 90 and 100 or between 0.9 and 1", info="wilson_upper error confidence > 100")
 })
 
