9f9f9da0faaa887e892a72150a2a278030ab551b
Georgina Anderson <georgina.anderson@phe.gov.uk>
2018-02-28 14:40:36 +0000

    Added elements to eliminate Check notes (added PHEindicatormethods.R and added README.Rmd and data-raw to buildignore)

---
+++ R/PHEindicatormethods.R
@@ -0,0 +1,32 @@
+#' PHEindicatormethods: A package for performing standard statistics for public
+#' health indicators
+#'
+#' @docType package
+#' @name PHEindicatormethods
+NULL
+globalVariables(c("esp2013",
+                  "exp_x",
+                  "expected",
+                  "lowercl",
+                  "method",
+                  "nrefpop_calc",
+                  "observed",
+                  "qchisq",
+                  "qnorm",
+                  "qt",
+                  "ref_rate",
+                  "sd",
+                  "sq_rate",
+                  "statistic",
+                  "stdev",
+                  "stdpop_calc",
+                  "total_count",
+                  "total_pop",
+                  "uppercl",
+                  "value",
+                  "value_count",
+                  "value_sum",
+                  "vardsr",
+                  "wt_rate",
+                  "xrefpop_calc")
+)
