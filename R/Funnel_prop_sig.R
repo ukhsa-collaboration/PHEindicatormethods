5e1c3ab3195768e96a151d50a37aaa4c4968f33f
Sebastian Fox <sebastian.fox@phe.gov.uk>
2021-06-22 17:36:20 +0100

    simplified phe_funnels significantly

--- R/Funnel_prop_sig.R
+++ R/Funnel_prop_sig.R
@@ -10,7 +10,7 @@
 #'          required condition (the numerator for the CLs); unquoted string; no default
 #' @param denominator field name from data containing the population(s) in the sample
 #'               (the denominator for the CLs); unquoted string; no default
-#'
+#' @param multiplier the multiplier used to express the final values (eg 100 = percentage); numeric; default 100
 #' @return returns the original data.frame with the following appended:
 #'         lower 0.025 limit, upper 0.025 limit, lower 0.001 limit, upper 0.001 limit and
 #'         baseline average
@@ -20,6 +20,7 @@
 #'
 #' @examples
 #' @export
+#' @author Matthew Francis, \email{Matthew.Francis@@phe.gov.uk}
 #'
 #' @family PHEindicatormethods package functions
 # -------------------------------------------------------------------------------------------------
@@ -28,7 +29,8 @@
 # NB -this does not alter the original dataset but generates a dataframe of 100 records for
 # plotting the control limits
 
-phe_fun_prop_plot<-function(data, x, denominator){
+phe_funnels <- function(data, x, denominator,
+                        multiplier = 100) {
 
   # check required arguments present
   if (missing(data) | missing(x) | missing(denominator)) {
@@ -36,81 +38,126 @@ phe_fun_prop_plot<-function(data, x, denominator){
   }
 
 
-  #calculate the initialisation variables - record level data
+  # calculate the initialisation variables - record level data
 
-  prop <- data %>%
-    mutate(prop = {{ x }} / {{ denominator }})
+  prop_calc <- data %>%
+    mutate(prop = 100 * {{ x }} / {{ denominator }}) %>%
+    pull()
 
-  prop <- prop$prop
-  prop_calc <- prop * 100
-
-  #aggregated data
+  # aggregated data
   summaries <- data %>%
-    summarise(av = sum({{ x }}) / sum({{ denominator }}),
-              mind = min({{ denominator }}),
-              maxd = max({{ denominator }}))
-  av <- summaries$av
-  mind <- summaries$mind
-  maxd <- summaries$maxd
+    summarise(
+      av = sum({{ x }}) / sum({{ denominator }}),
+      min_denominator = min({{ denominator }}),
+      max_denominator = max({{ denominator }})
+    )
 
-  if (maxd > 2 * mind) {
-    actmind <- 0
+  av <- summaries$av
+  min_denominator <- summaries$min_denominator
+  max_denominator <- summaries$max_denominator
+
+  if (max_denominator > 2 * min_denominator) {
+    axis_minimum <- 0
   } else {
-    actmind <- floor(mind * 0.95)
+    axis_minimum <- floor(min_denominator * 0.95)
   }
 
-  actmaxd <- ceiling((maxd * 1.05) /
-                       10 ^ (nchar(floor(maxd * 1.05 / 10) * 10) - 2)) * 10 ^ (nchar(floor(maxd * 1.05 / 10) * 10) - 2)
-  minp <- min(prop_calc)
-  maxp <- max(prop_calc)
+  axis_maximum <- ceiling((max_denominator * 1.05) /
+    10^(nchar(floor(max_denominator * 1.05 / 10) * 10) - 2)) * 10^(nchar(floor(max_denominator * 1.05 / 10) * 10) - 2)
+  min_percent <- min(prop_calc)
+  max_percent <- max(prop_calc)
 
-  if (maxp > 2 * minp) {
-    actminp <- 0
+  if (max_percent > 2 * min_percent) {
+    min_percent_axis <- 0
   } else {
-    actminp <- floor(minp * 0.95)
+    min_percent_axis <- floor(min_percent * 0.95)
   }
 
   # First create a vector with the numbers 1 to 100 (as the plot will have 100 data points)
-
   # Create a blank matrix which will be populated with results obtained above
 
-  t <- matrix(ncol = 7,
-              nrow = 100)
-  colnames(t) <- c("Row.number", "Population",
-                   "Lower2s0025limit", "Upper2s0025limit",
-                   "Lower3s0001limit", "Upper3s0001limit",
-                   "Baseline")
-  t[1,1] = 1
-  t[1,2] = 1
-  t[1,3] = max(0, ((av * (t[1, 2] / qnorm(0.975) ^ 2 + 1) -
-                      sqrt((- 8 * av * (t[1, 2] / qnorm(0.975) ^ 2 + 1)) ^ 2 - 64 *
-                             (1 / qnorm(0.975) ^ 2 + 1 / t[1, 2]) * av * (t[1, 2] *
-                                                                            (av * (t[1, 2] / qnorm(0.975) ^ 2 + 2) - 1) + qnorm(0.975) ^ 2 *
-                                                                            (av - 1))) / 8) / (1 / qnorm(0.975) ^ 2 + 1 / t[1, 2]) / t[1, 2]) * 100)
-  t[1,4] = min(100,((av*(t[1,2]/qnorm(0.975)^2+1)+sqrt((-8*av*(t[1,2]/qnorm(0.975)^2+1))^2-64*(1/qnorm(0.975)^2+1/t[1,2])*av*(t[1,2]*(av*(t[1,2]/qnorm(0.975)^2+2)-1)+qnorm(0.975)^2*
-                                                                                                                               (av-1)))/8)/(1/qnorm(0.975)^2+1/t[1,2])/t[1,2])*100)
-  t[1,5] = max(0,((av*(t[1,2]/qnorm(0.999)^2+1)-sqrt((-8*av*(t[1,2]/qnorm(0.999)^2+1))^2-64*(1/qnorm(0.999)^2+1/t[1,2])*av*(t[1,2]*(av*(t[1,2]/qnorm(0.999)^2+2)-1)+qnorm(0.999)^2*
-                                                                                                                             (av-1)))/8)/(1/qnorm(0.999)^2+1/t[1,2])/t[1,2])*100)
-  t[1,6] = min(100,((av*(t[1,2]/qnorm(0.999)^2+1)+sqrt((-8*av*(t[1,2]/qnorm(0.999)^2+1))^2-64*(1/qnorm(0.999)^2+1/t[1,2])*av*(t[1,2]*(av*(t[1,2]/qnorm(0.999)^2+2)-1)+qnorm(0.999)^2*
-                                                                                                                               (av-1)))/8)/(1/qnorm(0.999)^2+1/t[1,2])/t[1,2])*100)
-  t[1,7] = av * 100
+  t <- matrix(
+    ncol = 7,
+    nrow = 100
+  )
+  colnames(t) <- c(
+    "Row.number", "Population",
+    "Lower2s0025limit", "Upper2s0025limit",
+    "Lower3s0001limit", "Upper3s0001limit",
+    "Baseline"
+  )
+  t[1, "Row.number"] <- 1
+  t[1, "Population"] <- max(1, axis_minimum)
 
-  for(j in 2:100) {
-    t[j,1]=t[j-1]+1
-    t[j,2]=max(round((actmaxd/t[j-1,2])^(1/(101-t[j,1]))*t[j-1,2]),t[j-1,2]+1)
-    t[j,3]= max(0,((av*(t[j,2]/qnorm(0.975)^2+1)-sqrt((-8*av*(t[j,2]/qnorm(0.975)^2+1))^2-64*(1/qnorm(0.975)^2+1/t[j,2])*av*(t[j,2]*(av*(t[j,2]/qnorm(0.975)^2+2)-1)+qnorm(0.975)^2*
-                                                                                                                               (av-1)))/8)/(1/qnorm(0.975)^2+1/t[j,2])/t[j,2])*100)
-    t[j,4]= min(100,((av*(t[j,2]/qnorm(0.975)^2+1)+sqrt((-8*av*(t[j,2]/qnorm(0.975)^2+1))^2-64*(1/qnorm(0.975)^2+1/t[j,2])*av*(t[j,2]*(av*(t[j,2]/qnorm(0.975)^2+2)-1)+qnorm(0.975)^2*
-                                                                                                                                 (av-1)))/8)/(1/qnorm(0.975)^2+1/t[j,2])/t[j,2])*100)
-    t[j,5]= max(0,((av*(t[j,2]/qnorm(0.999)^2+1)-sqrt((-8*av*(t[j,2]/qnorm(0.999)^2+1))^2-64*(1/qnorm(0.999)^2+1/t[j,2])*av*(t[j,2]*(av*(t[j,2]/qnorm(0.999)^2+2)-1)+qnorm(0.999)^2*
-                                                                                                                               (av-1)))/8)/(1/qnorm(0.999)^2+1/t[j,2])/t[j,2])*100)
-    t[j,6]= min(100,((av*(t[j,2]/qnorm(0.999)^2+1)+sqrt((-8*av*(t[j,2]/qnorm(0.999)^2+1))^2-64*(1/qnorm(0.999)^2+1/t[j,2])*av*(t[j,2]*(av*(t[j,2]/qnorm(0.999)^2+2)-1)+qnorm(0.999)^2*                                                                                                                                 (av-1)))/8)/(1/qnorm(0.999)^2+1/t[j,2])/t[j,2])*100)
-    t[j,7]= av*100
+
+
+  sigma_adjustment <- function(table, p, population_row, average, side, multiplier) {
+    # ((av * (t[j, "Population"] / qnorm(0.999)^2 + 1) +
+    #     sigma_adjustment(t, 0.999, j) / 8) /
+    #    (1 / qnorm(0.999)^2 + 1 / t[j, "Population"]) /
+    #    t[j, "Population"]) * multiplier
+    first_part <- average * (table[population_row, "Population"] /
+                          qnorm(p)^2 + 1)
+
+    adj <- sqrt((-8 * av * (table[population_row, "Population"] /
+                              qnorm(p)^2 + 1))^2 - 64 *
+                  (1 / qnorm(p)^2 + 1 / table[population_row, "Population"]) *
+                  av * (table[population_row, "Population"] *
+                          (av * (table[population_row, "Population"] /
+                                   qnorm(p)^2 + 2) - 1) +
+                          qnorm(p)^2 *
+                          (av - 1)))
+
+    last_part <- (1 / qnorm(p)^2 + 1 /
+                    table[population_row, "Population"])
+
+    if (side == "low") {
+      adj_return <- (first_part - adj / 8) / last_part
+    } else if (side == "high") {
+      adj_return <- (first_part + adj / 8) / last_part
+    }
+    adj_return <- (adj_return /
+      table[population_row, "Population"]) * multiplier
+    return(adj_return)
   }
-  t<-as.data.frame(t)
-  t<-t%>%
-    select("Population","Lower2s0025limit","Upper2s0025limit","Lower3s0001limit",
-           "Upper3s0001limit","Baseline","Row.number")
+
+
+
+  t[1, "Lower2s0025limit"] <- max(0,
+                                  sigma_adjustment(t, 0.975, 1, av, "low", multiplier))
+  t[1, "Upper2s0025limit"] <- min(100,
+                                  sigma_adjustment(t, 0.975, 1, av, "high", multiplier))
+  t[1, "Lower3s0001limit"] <- max(0,
+                                  sigma_adjustment(t, 0.999, 1, av, "low", multiplier))
+  t[1, "Upper3s0001limit"] <- min(100,
+                                  sigma_adjustment(t, 0.999, 1, av, "high", multiplier))
+  t[1, "Baseline"] <- av * multiplier
+
+  for (j in 2:100) {
+
+    t[j, "Row.number"] <- t[j - 1] + 1
+    t[j, "Population"] <- max(round((axis_maximum / t[j - 1, "Population"])^(1 / (101 - t[j, "Row.number"])) *
+                                      t[j - 1, "Population"]),
+                              t[j - 1, "Population"] + 1)
+    t[j, "Lower2s0025limit"] <- max(0,
+                                    sigma_adjustment(t, 0.975, j, av, "low", multiplier))
+    t[j, "Upper2s0025limit"] <- min(100,
+                                    sigma_adjustment(t, 0.975, j, av, "high", multiplier))
+    t[j, "Lower3s0001limit"] <- max(0,
+                                    sigma_adjustment(t, 0.999, j, av, "low", multiplier))
+    t[j, "Upper3s0001limit"] <- min(100,
+                                    sigma_adjustment(t, 0.999, j, av, "high", multiplier))
+    t[j, "Baseline"] <- av * multiplier
+  }
+
+  t <- as.data.frame(t) %>%
+    select(
+      Population,
+      Lower2s0025limit, Upper2s0025limit,
+      Lower3s0001limit,
+      Upper3s0001limit, Baseline,
+      Row.number
+    )
 }
 
 
@@ -124,7 +171,7 @@ phe_fun_prop_plot<-function(data, x, denominator){
 #'          (the numerator for the CLs); unquoted string; no default
 #' @param denominator field name from data containing the population(s) in the sample (the denominator for the CLs);
 #'          unquoted string; no default
-#' @param multiplier the multiplier used to express the final values (eg 100 = percentage); numeric; default 1
+#' @param multiplier the multiplier used to express the final values (eg 100 = percentage); numeric; default 100
 #'
 #' @inheritParams phe_dsr
 #'
@@ -135,12 +182,13 @@ phe_fun_prop_plot<-function(data, x, denominator){
 #'
 #' @examples
 #' library(dplyr)
-#' df <- data.frame(Area= c("A", "B", "C", "D"),
-#'                  numerator = c(10232, 12321, 15123, 13213),
-#'                  denominator = c(15232, 16123, 17932, 18475))
+#' df <- data.frame(
+#'   Area = c("A", "B", "C", "D"),
+#'   numerator = c(10232, 12321, 15123, 13213),
+#'   denominator = c(15232, 16123, 17932, 18475)
+#' )
 #' df %>%
-#'     phe_fun_prop_sig(numerator, denominator)
-#'
+#'   phe_fun_prop_sig(numerator, denominator)
 #' @export
 #'
 #' @family PHEindicatormethods package functions
@@ -184,38 +232,38 @@ phe_funnel_significance <- function(data, x, denominator,
 
   # write the function
   dt_sig <- data %>%
-    mutate(low0_001 = pmax(0, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.999) ^ 2 + 1) -
-                                sqrt((- 8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.999) ^ 2 + 1)) ^ 2 -
-                                       64 * (1 / qnorm(0.999) ^ 2 + 1 / {{ denominator }}) * tot_av / multiplier *
-                                       ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.999) ^ 2 + 2) - 1)+
-                                          qnorm(0.999) ^ 2 * (tot_av / multiplier - 1))) /
-                                8) / (1 / qnorm(0.999) ^ 2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier),
-           low0_025 = pmax(0, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.975) ^ 2 + 1) -
-                                sqrt((-8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.975) ^ 2 + 1)) ^ 2 -
-                                       64 * (1 / qnorm(0.975) ^ 2 + 1 / {{ denominator }}) * tot_av / multiplier *
-                                       ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.975) ^ 2 + 2) - 1) +
-                                          qnorm(0.975) ^ 2 *(tot_av / multiplier - 1))) /
-                                8) / (1 / qnorm(0.975) ^ 2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier),
-           high0_001 = pmin(multiplier, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.999) ^ 2 + 1) +
-                                          sqrt((- 8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.999) ^ 2 + 1)) ^ 2 -
-                                                 64 * (1 / qnorm(0.999) ^ 2 + 1 / {{ denominator }}) * tot_av / multiplier *
-                                                 ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.999) ^ 2 + 2) - 1) +
-                                                    qnorm(0.999) ^ 2 * (tot_av / multiplier - 1))) /
-                                          8) / (1 / qnorm(0.999) ^ 2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier),
-           high0_025 = pmin(multiplier, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.975) ^ 2 + 1) +
-                                          sqrt((- 8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.975) ^ 2 + 1)) ^ 2 -
-                                                 64 * (1 / qnorm(0.975) ^ 2 + 1 / {{ denominator }}) * tot_av / multiplier *
-                                                 ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.975) ^ 2 + 2) - 1) +
-                                                    qnorm(0.975) ^ 2 * (tot_av / multiplier - 1))) /
-                                          8) / (1 / qnorm(0.975) ^ 2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier)) %>%
+    mutate(
+      low0_001 = pmax(0, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.999)^2 + 1) -
+        sqrt((-8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.999)^2 + 1))^2 -
+          64 * (1 / qnorm(0.999)^2 + 1 / {{ denominator }}) * tot_av / multiplier *
+            ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.999)^2 + 2) - 1) +
+              qnorm(0.999)^2 * (tot_av / multiplier - 1))) /
+          8) / (1 / qnorm(0.999)^2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier),
+      low0_025 = pmax(0, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.975)^2 + 1) -
+        sqrt((-8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.975)^2 + 1))^2 -
+          64 * (1 / qnorm(0.975)^2 + 1 / {{ denominator }}) * tot_av / multiplier *
+            ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.975)^2 + 2) - 1) +
+              qnorm(0.975)^2 * (tot_av / multiplier - 1))) /
+          8) / (1 / qnorm(0.975)^2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier),
+      high0_001 = pmin(multiplier, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.999)^2 + 1) +
+        sqrt((-8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.999)^2 + 1))^2 -
+          64 * (1 / qnorm(0.999)^2 + 1 / {{ denominator }}) * tot_av / multiplier *
+            ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.999)^2 + 2) - 1) +
+              qnorm(0.999)^2 * (tot_av / multiplier - 1))) /
+          8) / (1 / qnorm(0.999)^2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier),
+      high0_025 = pmin(multiplier, ((tot_av / multiplier * ({{ denominator }} / qnorm(0.975)^2 + 1) +
+        sqrt((-8 * tot_av / multiplier * ({{ denominator }} / qnorm(0.975)^2 + 1))^2 -
+          64 * (1 / qnorm(0.975)^2 + 1 / {{ denominator }}) * tot_av / multiplier *
+            ({{ denominator }} * (tot_av / multiplier * ({{ denominator }} / qnorm(0.975)^2 + 2) - 1) +
+              qnorm(0.975)^2 * (tot_av / multiplier - 1))) /
+          8) / (1 / qnorm(0.975)^2 + 1 / {{ denominator }}) / {{ denominator }}) * multiplier)
+    ) %>%
     mutate(significance = case_when(
-      {{ x }} * {{ denominator }} * multiplier * low0_001  ~ "Low (0.001)",
-      {{ x }} * {{ denominator }} * multiplier * low0_025  ~ "Low (0.025)",
+      {{ x }} * {{ denominator }} * multiplier * low0_001 ~ "Low (0.001)",
+      {{ x }} * {{ denominator }} * multiplier * low0_025 ~ "Low (0.025)",
       {{ x }} * {{ denominator }} * multiplier * high0_001 ~ "High (0.001)",
       {{ x }} * {{ denominator }} * multiplier * high0_025 ~ "High (0.025)",
-      TRUE ~ "")
-    ) %>%
+      TRUE ~ ""
+    )) %>%
     select(!c(low0_001, low0_025, high0_001, high0_025))
-
 }
-
