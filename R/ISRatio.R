16e31dd8dd369cccf7f85af428254fc91fcefb30
(DHSC) Annabel Westermann <Annabel.Westermann@phe.gov.uk>
2023-04-20 09:08:48 +0100

    update tests

--- R/ISRatio.R
+++ R/ISRatio.R
@@ -89,206 +89,194 @@
 #' @family PHEindicatormethods package functions
 # -------------------------------------------------------------------------------------------------
 
-
-library(dplyr)
-df <- data.frame(indicatorid = rep(c(1234, 5678, 91011, 121314), each = 19 * 2 * 5),
-                 year = rep(2006:2010, each = 19 * 2),
-                 sex = rep(rep(c("Male", "Female"), each = 19), 5),
-                 ageband = rep(c(0,5,10,15,20,25,30,35,40,45,
-                                 50,55,60,65,70,75,80,85,90), times = 10),
-                 obs = sample(200, 19 * 2 * 5 * 4, replace = TRUE),
-                 pop = sample(10000:20000, 19 * 2 * 5 * 4, replace = TRUE))
-
-refdf <- data.frame(refcount = sample(200, 19, replace = TRUE),
-                    refpop = sample(10000:20000, 19, replace = TRUE))
-
-## Calculate ISR when observed totals aren't available with age-breakdowns
-observed_totals <- data.frame(indicatorid = rep(c(1234, 5678, 91011, 121314), each = 10),
-                              year = rep(rep(2006:2010, each = 2),4),
-                              sex = rep(rep(c("Male", "Female"), each = 1),20),
-                              observed = sample(1500:2500, 40))
-
-df %>%
-  group_by(indicatorid, year, sex) %>%
-  calculate_ISRatio(observed, pop, refdf$refcount, refdf$refpop,
-                   observed_totals = observed_totals)
-
-
 calculate_ISRatio <- function(data, x, n, x_ref, n_ref, refpoptype = "vector",
-                    type = "full", confidence = 0.95, refvalue = 1,
-                    observed_totals = NULL) {
+                              type = "full", confidence = 0.95, refvalue = 1,
+                              observed_totals = NULL) {
 
-    # check required arguments present
-    if (missing(data)|missing(x)|missing(n)|missing(x_ref)|missing(n_ref)) {
-        stop("function calculate_ISRatio requires at least 5 arguments: data, x, n, x_ref and n_ref")
-    }
+  # check required arguments present
+  if (missing(data)|missing(x)|missing(n)|missing(x_ref)|missing(n_ref)) {
+    stop("function calculate_ISRatio requires at least 5 arguments: data, x, n, x_ref and n_ref")
+  }
 
-    # check same number of rows per group
+  # check same number of rows per group - if data is used ### NOT SURE WHY THIS IS ADDED?
+  if (!is.null(data)) {
     if (n_distinct(select(ungroup(count(data)),n)) != 1) {
-        stop("data must contain the same number of rows for each group")
+      stop("data must contain the same number of rows for each group")
     }
+  }
 
-    # check x is in data/observed_totals
+  # check x is in data/observed_totals
+  if (!is.null(observed_totals)) {
+    if (!(deparse(substitute(x)) %in% colnames(observed_totals))) {
+      stop("observed_totals is provided but x is not a field name in it")
+    }
+  } else {
+    if (!(deparse(substitute(x)) %in% colnames(data))) {
+      stop("x is not in data")
+    }
+  }
+
+  # check ref pops are valid and append to data
+  if (!(refpoptype %in% c("vector","field"))) {
+    stop("valid values for refpoptype are vector and field")
+
+  } else if (refpoptype == "vector") {
+    if (pull(slice(select(ungroup(count(data)),"n"),1)) != length(x_ref)) {
+      stop("x_ref length must equal number of rows in each group within data")
+    } else if (pull(slice(select(ungroup(count(data)),"n"),1)) != length(n_ref)) {
+      stop("n_ref length must equal number of rows in each group within data")
+    }
+    data <- mutate(data,xrefpop_calc = x_ref,
+                   nrefpop_calc = n_ref)
+
+  } else if (refpoptype == "field") {
+    if (deparse(substitute(x_ref)) %in% colnames(data)) {
+      if(deparse(substitute(n_ref)) %in% colnames(data)) {
+        data <- mutate(data,xrefpop_calc = {{ x_ref }},
+                       nrefpop_calc = {{ n_ref }})
+      } else stop("n_ref is not a field name from data")
+    } else stop("x_ref is not a field name from data")
+  }
+
+
+  # validate arguments
+  if (is.null(observed_totals)) {
+    if (any(pull(data, {{ x }}) < 0, na.rm=TRUE)) {
+      stop("numerators must all be greater than or equal to zero")
+    }
+  } else {
+    if (any(pull(observed_totals, {{ x }}) < 0, na.rm=TRUE)) {
+      stop("numerators must all be greater than or equal to zero")
+    }
+  }
+
+  if (any(pull(data, {{ n }}) < 0, na.rm = TRUE)) {
+    stop("denominators must all be greater than or equal to zero")
+  } else if (any(pull(data, {{ n }}) < 0, na.rm = TRUE)) {
+    stop("denominators must all be greater than or equal to zero")
+  } else if (!(type %in% c("value", "lower", "upper", "standard", "full"))) {
+    stop("type must be one of value, lower, upper, standard or full")
+  } else if (length(confidence) >2) {
+    stop("a maximum of two confidence levels can be provided")
+  } else if (length(confidence) == 2) {
+    if (!(confidence[1] == 0.95 & confidence[2] == 0.998)) {
+      stop("two confidence levels can only be produced if they are specified as 0.95 and 0.998")
+    }
+  } else if ((confidence < 0.9)|(confidence > 1 & confidence < 90)|(confidence > 100)) {
+    stop("confidence level must be between 90 and 100 or between 0.9 and 1")
+  }
+
+  # Identify join columns if observed events provided as totals
+  if (!is.null(observed_totals)) {
+    observed_total_join_cols <- base::intersect(colnames(data),
+                                                colnames(observed_totals))
+  }
+
+
+  # calculate isr and cis and populate metadata fields
+  if (length(confidence) == 2) {
+
+    # if two confidence levels requested
+    conf1 <- confidence[1]
+    conf2 <- confidence[2]
+
+    # calculate isr and CIs
+
     if (!is.null(observed_totals)) {
-      if (!(deparse(substitute(x)) %in% colnames(observed_totals))) {
-        stop("observed_totals is provided but x is not a field name in it")
-      }
+      ISRatio <- data %>%
+        mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
+        summarise(expected = sum(.data$exp_x),
+                  .groups  = "keep") %>%
+        left_join(observed_totals, by = observed_total_join_cols) %>%
+        rename("observed" = {{ x }}) %>%
+        select("observed", "expected")
     } else {
-      if (!(deparse(substitute(x)) %in% colnames(data))) {
-        stop("x is not in data")
-      }
+      ISRatio <- data %>%
+        mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
+        summarise(observed  = sum({{ x }}, na.rm = TRUE),
+                  expected  = sum(.data$exp_x),
+                  .groups = "keep")
     }
 
-    # check ref pops are valid and append to data
-    if (!(refpoptype %in% c("vector","field"))) {
-        stop("valid values for refpoptype are vector and field")
-
-    } else if (refpoptype == "vector") {
-      if (pull(slice(select(ungroup(count(data)),"n"),1)) != length(x_ref)) {
-          stop("x_ref length must equal number of rows in each group within data")
-      } else if (pull(slice(select(ungroup(count(data)),"n"),1)) != length(n_ref)) {
-          stop("n_ref length must equal number of rows in each group within data")
-      }
-      data <- mutate(data,xrefpop_calc = x_ref,
-                          nrefpop_calc = n_ref)
-
-    } else if (refpoptype == "field") {
-        if (deparse(substitute(x_ref)) %in% colnames(data)) {
-            if(deparse(substitute(n_ref)) %in% colnames(data)) {
-                data <- mutate(data,xrefpop_calc = {{ x_ref }},
-                                    nrefpop_calc = {{ n_ref }})
-            } else stop("n_ref is not a field name from data")
-        } else stop("x_ref is not a field name from data")
-    }
-
-
-    # validate arguments
-
-    if (is.null(observed_totals)) {
-      if (any(pull(data, {{ x }}) < 0, na.rm=TRUE)) {
-        stop("numerators must all be greater than or equal to zero")
-      }
-    } else {
-      if (any(pull(observed_totals, {{ x }}) < 0, na.rm=TRUE)) {
-        stop("numerators must all be greater than or equal to zero")
-      }
-    }
-
-    if (any(pull(data, {{ n }}) < 0, na.rm=TRUE)) {
-      stop("denominators must all be greater than or equal to zero")
-    } else if (!(type %in% c("value", "lower", "upper", "standard", "full"))) {
-      stop("type must be one of value, lower, upper, standard or full")
-    } else if (length(confidence) >2) {
-      stop("a maximum of two confidence levels can be provided")
-    } else if (length(confidence) == 2) {
-      if (!(confidence[1] == 0.95 & confidence[2] == 0.998)) {
-        stop("two confidence levels can only be produced if they are specified as 0.95 and 0.998")
-      }
-    } else if ((confidence < 0.9)|(confidence > 1 & confidence < 90)|(confidence > 100)) {
-      stop("confidence level must be between 90 and 100 or between 0.9 and 1")
-    }
-
-
-    # Identify join columns if observed events provided as totals
-    if (!is.null(observed_totals)) {
-      observed_total_join_cols <- base::intersect(colnames(data),
-                                                  colnames(observed_totals))
-    }
-
-
-    # calculate isr and cis and populate metadata fields
-    if (length(confidence) == 2) {
-
-        # if two confidence levels requested
-        conf1 <- confidence[1]
-        conf2 <- confidence[2]
-
-        # calculate isr and CIs
-        if (!is.null(observed_totals)) {
-          ISRatio <- data %>%
-            mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
-            summarise(expected  = sum(.data$exp_x),
-                      .groups = "keep") %>%
-            left_join(observed_totals, by = observed_total_join_cols) %>%
-            rename("observed" = {{ x }}) %>%
-            select("observed", "expected", "ref_rate")
-        } else {
-          ISRatio <- data %>%
-            mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
-            summarise(observed  = sum({{ x }}, na.rm = TRUE),
-                      expected  = sum(.data$exp_x),
-                      .groups = "keep")
-        }
-        ISRatio <- ISRatio %>%
-          mutate(value     = .data$observed / .data$expected * refvalue,
-                 lower95_0cl = if_else(.data$observed < 10, qchisq((1-conf1)/2,2*.data$observed)/2/.data$expected * refvalue,
+    ISRatio <- ISRatio %>%
+      mutate(value     = .data$observed / .data$expected * refvalue,
+             lower95_0cl = if_else(.data$observed < 10, qchisq((1-conf1)/2,2*.data$observed)/2/.data$expected * refvalue,
                                    byars_lower(.data$observed,conf1)/.data$expected * refvalue),
-                 upper95_0cl = if_else(.data$observed < 10, qchisq(conf1+(1-conf1)/2,2*.data$observed+2)/2/.data$expected * refvalue,
+             upper95_0cl = if_else(.data$observed < 10, qchisq(conf1+(1-conf1)/2,2*.data$observed+2)/2/.data$expected * refvalue,
                                    byars_upper(.data$observed,conf1)/.data$expected * refvalue),
-                 lower99_8cl = if_else(.data$observed < 10, qchisq((1-conf2)/2,2*.data$observed)/2/.data$expected * refvalue,
+             lower99_8cl = if_else(.data$observed < 10, qchisq((1-conf2)/2,2*.data$observed)/2/.data$expected * refvalue,
                                    byars_lower(.data$observed,conf2)/.data$expected * refvalue),
-                 upper99_8cl = if_else(.data$observed < 10, qchisq(conf2+(1-conf2)/2,2*.data$observed+2)/2/.data$expected * refvalue,
+             upper99_8cl = if_else(.data$observed < 10, qchisq(conf2+(1-conf2)/2,2*.data$observed+2)/2/.data$expected * refvalue,
                                    byars_upper(.data$observed,conf2)/.data$expected * refvalue),
-                 confidence = paste(conf1 * 100, "%, ", conf2 * 100, "%", sep=""),
-                 statistic = paste("indirectly standardised ratio x ",format(refvalue, scientific=F), sep=""),
-                 method  = if_else(.data$observed < 10, "Exact", "Byars"))
+             confidence = paste(conf1 * 100, "%, ", conf2 * 100, "%", sep=""),
+             statistic = paste("indirectly standardised ratio x ",format(refvalue, scientific=F), sep=""),
+             method  = if_else(.data$observed < 10, "Exact", "Byars"))
 
-        # drop fields not required based on type argument
-        if (type == "lower") {
-          ISRatio <- ISRatio %>%
-            select(!c("observed", "expected", "value", "upper95_0cl", "upper99_8cl", "confidence", "statistic", "method"))
-        } else if (type == "upper") {
-          ISRatio <- ISRatio %>%
-            select(!c("observed", "expected", "value", "lower95_0cl", "lower99_8cl", "confidence", "statistic", "method"))
-        } else if (type == "value") {
-          ISRatio <- ISRatio %>%
-            select(!c("observed", "expected", "lower95_0cl", "upper95_0cl", "lower99_8cl", "upper99_8cl", "confidence", "statistic", "method"))
-        } else if (type == "standard") {
-          ISRatio <- ISRatio %>%
-            select(!c("confidence", "statistic", "method"))
-        }
-
-    } else {
-
-      # scale confidence level
-      if (confidence[1] >= 90) {
-        confidence <- confidence/100
-      }
-
-      # calculate ISR and a single CI
-        ISRatio <- data %>%
-            mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
-            summarise(observed = sum({{ x }}, na.rm = TRUE),
-                  expected     = sum(.data$exp_x),
-                  .groups = "keep") %>%
-            mutate(value      = .data$observed / .data$expected * refvalue,
-                   lowercl    = if_else(.data$observed < 10, qchisq((1-confidence)/2,2*.data$observed)/2/.data$expected * refvalue,
-                                    byars_lower(.data$observed,confidence)/.data$expected * refvalue),
-                   uppercl    = if_else(.data$observed < 10, qchisq(confidence+(1-confidence)/2,2*.data$observed+2)/2/.data$expected * refvalue,
-                                    byars_upper(.data$observed,confidence)/.data$expected * refvalue),
-                   confidence = paste(confidence * 100, "%", sep=""),
-                   statistic  = paste("indirectly standardised ratio x ",format(refvalue, scientific=F), sep=""),
-                   method     = if_else(.data$observed < 10, "Exact", "Byars"))
-
-        # drop fields not required based on type argument
-        if (type == "lower") {
-          ISRatio <- ISRatio %>%
-            select(!c("observed", "expected", "value", "uppercl", "confidence", "statistic", "method"))
-        } else if (type == "upper") {
-          ISRatio <- ISRatio %>%
-            select(!c("observed", "expected", "value", "lowercl", "confidence", "statistic", "method"))
-        } else if (type == "value") {
-          ISRatio <- ISRatio %>%
-            select(!c("observed", "expected", "lowercl", "uppercl", "confidence", "statistic", "method"))
-        } else if (type == "standard") {
-          ISRatio <- ISRatio %>%
-            select(!c("confidence", "statistic", "method"))
-        }
-
+    # drop fields not required based on type argument
+    if (type == "lower") {
+      ISRatio <- ISRatio %>%
+        select(!c("observed", "expected", "value", "upper95_0cl", "upper99_8cl", "confidence", "statistic", "method"))
+    } else if (type == "upper") {
+      ISRatio <- ISRatio %>%
+        select(!c("observed", "expected", "value", "lower95_0cl", "lower99_8cl", "confidence", "statistic", "method"))
+    } else if (type == "value") {
+      ISRatio <- ISRatio %>%
+        select(!c("observed", "expected", "lower95_0cl", "upper95_0cl", "lower99_8cl", "upper99_8cl", "confidence", "statistic", "method"))
+    } else if (type == "standard") {
+      ISRatio <- ISRatio %>%
+        select(!c("confidence", "statistic", "method"))
     }
 
-    return(ISRatio)
+  } else {
+
+    # scale confidence level
+    if (confidence[1] >= 90) {
+      confidence <- confidence/100
+    }
+
+    # calculate ISR and a single CI
+    if (!is.null(observed_totals)) {
+      ISRatio <- data %>%
+        mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
+        summarise(expected = sum(.data$exp_x),
+                  .groups  = "keep") %>%
+        left_join(observed_totals, by = observed_total_join_cols) %>%
+        rename("observed" = {{ x }}) %>%
+        select("observed", "expected")
+    } else {
+      ISRatio <- data %>%
+        mutate(exp_x = na.zero(.data$xrefpop_calc) / .data$nrefpop_calc * na.zero({{ n }})) %>%
+        summarise(observed = sum({{ x }}, na.rm = TRUE),
+                  expected     = sum(.data$exp_x),
+                  .groups = "keep")
+    }
+
+    ISRatio <- ISRatio %>%
+      mutate(value      = .data$observed / .data$expected * refvalue,
+             lowercl    = if_else(.data$observed < 10, qchisq((1-confidence)/2,2*.data$observed)/2/.data$expected * refvalue,
+                                  byars_lower(.data$observed,confidence)/.data$expected * refvalue),
+             uppercl    = if_else(.data$observed < 10, qchisq(confidence+(1-confidence)/2,2*.data$observed+2)/2/.data$expected * refvalue,
+                                  byars_upper(.data$observed,confidence)/.data$expected * refvalue),
+             confidence = paste(confidence * 100, "%", sep=""),
+             statistic  = paste("indirectly standardised ratio x ",format(refvalue, scientific=F), sep=""),
+             method     = if_else(.data$observed < 10, "Exact", "Byars"))
+
+    # drop fields not required based on type argument
+    if (type == "lower") {
+      ISRatio <- ISRatio %>%
+        select(!c("observed", "expected", "value", "uppercl", "confidence", "statistic", "method"))
+    } else if (type == "upper") {
+      ISRatio <- ISRatio %>%
+        select(!c("observed", "expected", "value", "lowercl", "confidence", "statistic", "method"))
+    } else if (type == "value") {
+      ISRatio <- ISRatio %>%
+        select(!c("observed", "expected", "lowercl", "uppercl", "confidence", "statistic", "method"))
+    } else if (type == "standard") {
+      ISRatio <- ISRatio %>%
+        select(!c("confidence", "statistic", "method"))
+    }
+
+  }
+
+  return(ISRatio)
 }
-
-
-
